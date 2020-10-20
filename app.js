a94fd667c25f2da4d0149141570fa35228048b09
pratiklp <37157850+pratiklp@users.noreply.github.com>
2020-10-20 03:06:22 -0400

    Added a test scene in app.js

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ class App {
         const container = document.createElement('div');
         document.body.appendChild(container);
 
+        this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 100);
+        this.camera.position.set(0, 0, 4);
+
+        this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color(255, 0, 0);
+
+        this.renderer = new THREE.WebGLRenderer({ antialias: true });
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        container.appendChild(this.renderer.domElement);
+
+        this.renderer.setAnimationLoop(this.render.bind(this));
+
         window.addEventListener('resize', this.resize.bind(this));
     }
 
@@ -14,7 +27,7 @@ class App {
     }
 
     render() {
-
+        this.renderer.render(this.scene, this.camera);
     }
 }
 export { App };
