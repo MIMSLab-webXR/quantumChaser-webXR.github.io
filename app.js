0b44d51f6525d340d26971717eb391a161219c78
pratiklp <37157850+pratiklp@users.noreply.github.com>
2020-10-20 02:52:34 -0400

    Added a class outline on the app.js

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 import * as THREE from './Library/THREE/three.module.js';
 import { OrbitControls } from './Library/THREE/jsm/OrbitControls.js';
+
+class App {
+    constructor() {
+        const container = document.createElement('div');
+        document.body.appendChild(container);
+
+        window.addEventListener('resize', this.resize.bind(this));
+    }
+
+    resize() {
+
+    }
+
+    render() {
+
+    }
+}
+export { App };
