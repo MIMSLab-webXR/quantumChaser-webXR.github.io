35a94e103a5f071d9238a21adb5e4644aad692e3
pratiklp <37157850+pratiklp@users.noreply.github.com>
2020-10-20 03:29:18 -0400

    Added a green rotating cube along with lighting, black background, touch input, and screen size adjustment based on Udemy WebXR Lesson 2-1

--- app.js
+++ app.js
@@ -10,7 +10,14 @@ class App {
         this.camera.position.set(0, 0, 4);
 
         this.scene = new THREE.Scene();
-        this.scene.background = new THREE.Color(255, 0, 0);
+        this.scene.background = new THREE.Color(0, 0, 0);
+
+        const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3);
+        this.scene.add(ambient);
+
+        const light = new THREE.DirectionalLight();
+        light.position.set(0.2, 1, 1);
+        this.scene.add(light);
 
         this.renderer = new THREE.WebGLRenderer({ antialias: true });
         this.renderer.setPixelRatio(window.devicePixelRatio);
@@ -19,14 +26,24 @@ class App {
 
         this.renderer.setAnimationLoop(this.render.bind(this));
 
+        const geometry = new THREE.BoxBufferGeometry();
+        const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+
+        this.scene.add(this.mesh);
+
+        const controls = new OrbitControls(this.camera, this.renderer.domElement);
+
         window.addEventListener('resize', this.resize.bind(this));
     }
 
     resize() {
-
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
     }
 
     render() {
+        this.mesh.rotateY(0.01);
         this.renderer.render(this.scene, this.camera);
     }
 }
