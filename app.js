013825c2441e2e6c625515064c139d7ce5a58228
pratiklp <37157850+pratiklp@users.noreply.github.com>
2020-10-26 00:44:15 -0400

    added controller input support

--- app.js
+++ app.js
@@ -102,7 +102,10 @@ class App {
     }
 
     initScene() {
-
+        this.highlight = new THREE > Mesh(geometry,
+            new THREE.MeshBasicMaterial({ color: 0xFFFFFF }));
+        this.highlight.scale.set(1.2, 1.2, 1.2);
+        this.scene.add(this.highlight);
     }
 
     setupVR() {
@@ -111,6 +114,22 @@ class App {
         document.body.appendChild(VRButton.createButton(this.renderer));
 
         this.controllers = this.buildControllers();
+
+        function onSelectStart() {
+            this.children[0].scale.z = 10;
+            this.userData.selectPressed = true;
+        }
+
+        function onSelectEnd() {
+            this.children[0].scale.z = 0;
+            self.highlight.visible = false;
+            this.userData.selectPressed = false;
+        }
+
+        this.controllers.forEach((controller) => {
+            controller.addEventListener('selectstart', onSelectStart);
+            controller.addEventListener('selectend', onSelectEnd);
+        });
     }
 
     setupAR() {
@@ -153,7 +172,24 @@ class App {
     }
 
     handleController(controller) {
+        if (controller.userData.selectPressed) {
+            controller.children[0].scale.z = 10;
 
+            this.workingMatrix.identity().extractRotation(controller.matrixWorld);
+            this.raycaster.ray.origin.setFromMatrixPosition(controller.matrixWorld);
+
+            this.raycaster.ray.direction.set(0, 0, -1).applyMatrix4(this.workingMatrix);
+
+            const intersects = this.raycaster.intersectObjects(this.room.children);
+
+            if (intersects.length > 0) {
+                intersects[0].object.add(this.highlight);
+                this.highlight.visible = true;
+                controller.children[0].scale.z = intersects[0].distance;
+            } else {
+                this.highlight.visible = false;
+            }
+        }
     }
 
     render() {
