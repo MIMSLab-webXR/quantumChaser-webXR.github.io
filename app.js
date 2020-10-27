6351e66ed1113d2a9a164de443717d8610edd596
pratiklp <37157850+pratiklp@users.noreply.github.com>
2020-10-27 16:57:19 -0400

    VR controller position fix

--- app.js
+++ app.js
@@ -225,7 +225,7 @@ class App {
         switch (data.targetRayMode) {
             case 'tracked-pointer':
                 geometry = new THREE.BufferGeometry();
-                geometry.setAttribute('position', new THREE.Float32BufferAttribute([0, 0, 0, 0, 0, -1], 3));
+                geometry.setAttribute('position', new THREE.Float32BufferAttribute([0, 0, 0, 0, 0, - 1], 3));
                 geometry.setAttribute('color', new THREE.Float32BufferAttribute([0.5, 0.5, 0.5, 0, 0, 0], 3));
 
                 material = new THREE.LineBasicMaterial({
