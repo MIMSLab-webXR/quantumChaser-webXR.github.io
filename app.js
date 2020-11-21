e4fdfb1e04bbb05173a09e4cb18e0b9743ed0f02
pratiklp <37157850+pratiklp@users.noreply.github.com>
2020-11-21 00:01:18 -0500

    added a skybox and lighting

--- app.js
+++ app.js
@@ -22,343 +22,114 @@ import {
     MotionController
 } from './Library/THREE/jsm/motion-controllers.module.js';
 
-//const DEFAULT_PROFILES_PATH = 'https://cdn.jsdelivr.net/npm/@webze-input-profiles/assets@1.0/dist/profiles';
-//const DEFAULT_PROFILE = "generic-trigger";
-
 class App {
     constructor() {
+        // Creating a container for the application
+
         const container = document.createElement('div');
         document.body.appendChild(container);
 
-        this.assetsPath = './Assets/';
-
-        this.camera = new THREE.PerspectiveCamera(60,
-            window.innerWidth / window.innerHeight, 0.1, 3000);
-        this.camera.position.set(0, 1.6, 5);
+        // Setting up a scene
 
         this.scene = new THREE.Scene();
 
-        const ambient = new THREE.HemisphereLight(0x555555, 0x999999);
-        this.scene.add(ambient);
+        // Setting up a camera
 
-        this.sun = new THREE.DirectionalLight(0xAAAAFF, 2.5);
-        this.sun.castShadow = true;
+        this.camera = new THREE.PerspectiveCamera(
+            75, // FOV
+            window.innerWidth / window.innerHeight, // Aspect Ratio
+            0.1, // Nearest plane
+            1000 // Farthest plane
+        )
 
-        const lightSize = 5;
-        this.sun.shadow.camera.near = 0.1;
-        this.sun.shadow.camera.far = 17;
-        this.sun.shadow.camera.left = this.sun.shadow.camera.bottom = -lightSize;
-        this.sun.shadow.camera.right = this.sun.shadow.camera.top = lightSize;
+        // Setting up a WebGL renderer
 
-        this.sun.shadow.mapSize.width = 1024;
-        this.sun.shadow.mapSize.height = 1024;
-
-        this.sun.position.set(0, 10, 10);
-        this.scene.add(this.sun);
-
-        this.renderer = new THREE.WebGLRenderer({ antialias: true });
+        this.renderer = new THREE.WebGL1Renderer();
         this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.renderer.shadowMap.enabled = true;
-        this.renderer.outputEncoding = THREE.sRGBEncoding;
-        container.appendChild(this.renderer.domElement);
-        this.setEnvironment();
+        this.renderer.outputEncoding = new THREE.sRGBEncoding;
+        document.body.appendChild(this.renderer.domElement);
 
-        //this.controls = new OrbitControls(
-        //    this.camera,
-        //    this.renderer.domElement);
-        //this.controls.target.set(0, 1.6, 0);
-        //this.controls.update();
+        // Setting up lighting
 
-        this.stats = new Stats();
+        // 1 - Directional Light: Emitted in a specific direction; emulates sunrays
+        // Inputs - color, intensity
+
+        this.directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.5);
+        this.scene.add(this.directionalLight);
+        this.directionalLight.position.set(0, 10, 0);
+
+        // Other properties include .castShadow (expensive, boolean),
+        // .shadow (calculate shadows for light)
+        // .target (targeted object for the light effect)
+
+        // 2 - Ambient Light: Illuminates all present objects in the scene equally
+
+        this.ambient = new THREE.AmbientLight(0x404040);
+        this.scene.add(this.light);
+
+        // 3 - Hemisphere Light: Positioned directly above the scene, 
+        // with color fading from the sky color to the ground color
+
+        this.hemisphereLight = new THREE.HemisphereLight(0xFFFFBB, 0X080820);
+        this.scene.add(this.hemisphereLight);
+        this.hemisphereLight.position(0, 10, 0);
+
+        // FPS count - Realtime
+
+        this.fpvCounter = new Stats();
         document.body.appendChild(this.stats.dom);
 
+        // Setting up an Orbit Control point to allow the camera to orient
+        // per the head movement
+
+        this.orbitControl = new OrbitControls(this.camera, this.renderer.domElement);
+        this.orbitControl.target.set(0, 3.5, 0);
+        this.orbitControl.update();
+
+        // Raycaster setting - Raycasting is used for input picking 
+        // (working out what objects in the 3D space the input pointer 
+        // is pointing towards).
+
         this.raycaster = new THREE.Raycaster();
+
+        // Setting up a working matrix
+
         this.workingMatrix = new THREE.Matrix4();
+
+        //Setting up a working vector
+
         this.workingVector = new THREE.Vector3();
-        this.origin = new THREE.Vector3();
 
-        this.clock = new THREE.Clock();
+        // Calling the objects and actors
 
-        this.loadingBar = new LoadingBar();
-
-        this.loadEnvironment();
-
-        this.loading = true;
+        this.initScene();
+        this.setupXR();
 
         window.addEventListener('resize', this.resize.bind(this));
     }
 
-    setEnvironment() {
-        const loader = new RGBELoader().setDataType(THREE.UnsignedByteType);
-        const pmremGenerator = new THREE.PMREMGenerator(this.renderer);
-        pmremGenerator.compileEquirectangularShader();
+    initScene() {
+        // Layer 1 - Sky shader
 
-        const self = this;
-
-        loader.load('./Assets/TrainingKits/hdr/venice_sunset_1k.hdr', (texture) => {
-            const envMap = pmremGenerator.fromEquirectangular(texture).texture;
-            pmremGenerator.dispose();
-
-            self.scene.environment = envMap;
-
-        }, undefined, (err) => {
-            console.error('An error occured setting the environment');
-        });
+        this.textureSky = new THREE.TextureLoader().load('./Images/Sky.jpg');
+        this.geometryUniverse = new THREE.BoxBufferGeometry(100, 100, 100);
+        this.materialUniverse = new THREE.MeshBasicMaterial({ map: this.textureSky, side: THREE.BackSide });
+        this.universe = new THREE.Mesh(this.geometryUniverse, this.materialUniverse);
+        this.scene.add(this.universe);
     }
 
-    loadEnvironment() {
+    updateScene() {
 
-        const loader = new GLTFLoader().setPath(this.assetsPath);
-        const self = this;
-
-        // Load a glTF resource
-        loader.load(
-            // resource URL
-            './TrainingKits/dungeon.glb',
-            // called when the resource is loaded
-            function (gltf) {
-                const scale = 0.5;
-
-                self.scene.add(gltf.scene);
-
-                gltf.scene.traverse(function (child) {
-                    let interactable;
-                    if (child.isMesh) {
-                        if (child.name == "Navmesh") {
-                            child.material.visible = false;
-                            self.navmesh = child;
-                            child.geometry.scale(scale, scale, scale);
-                            child.scale.set(2, 2, 2);
-                        } else if (child.name == "SD_Prop_Chest_Skull_Lid_01") {
-                            self.interactables.push(new Interactable(child, {
-                                mode: 'tweens',
-                                tweens: [{
-                                    target: child.quaternion,
-                                    channel: 'x',
-                                    start: 0,
-                                    end: -0.7,
-                                    duration: 1
-                                }]
-                            }));
-                        } else if (child.name == "Door_01") {
-                            self.interactables.push(new Interactable(child, {
-                                mode: 'tweens',
-                                tweens: [{
-                                    target: chold.quaternion,
-                                    channel: 'z',
-                                    start: 0,
-                                    end: 0.6,
-                                    duration: 1
-                                }]
-                            }));
-                        }
-                    }
-                });
-
-                gltf.scene.scale.set(scale, scale, scale);
-
-                self.initGame();
-            },
-            // called while loading is progressing
-            function (xhr) {
-
-                self.loadingBar.progress = (xhr.loaded / xhr.total);
-
-            }/*,
-            // called when loading has errors
-            function (error) {
-
-                console.log('An error happened');
-
-            }*/
-        );
-    }			
-
-    initGame() {
-        this.player = this.createPlayer();
-
-        const locations = [
-            new THREE.Vector3(-0.409, 0.086, 4.038),
-            new THREE.Vector3(-0.846, 0.112, 5.777),
-            new THREE.Vector3(5.220, 0.176, 2.677),
-            new THREE.Vector3(1.490, 2.305, -1.599),
-            new THREE.Vector3(7.565, 2.694, 0.008),
-            new THREE.Vector3(-8.417, 2.676, 0.192),
-            new THREE.Vector3(-6.644, 2.600, -4.114)
-        ];
-
-        const self = this;
-
-        this.teleports = [];
-        locations.forEach(location => {
-            const teleport = new TeleportMesh();
-            teleport.position.copy(location);
-            self.scene.add(teleport);
-            self.teleports.push(teleport);
-        })
-
-        this.setupXR();
-
-        this.loading = false;
-
-        this.renderer.setAnimationLoop(this.render.bind(this));
-
-        this.loadingBar.visible = false;
-    }
-
-    createMarker(geometry, material) {
-        const mesh = new THREE.Mesh(geometry, material);
-        mesh.visible = false;
-        this.scene.add(mesh);
-        return mesh;
-    }
-
-    buildControllers() {
-        const controllerModelFactory = new XRControllerModelFactory();
-
-        const geometry = new THREE.BufferGeometry().setFromPoints([
-            new THREE.Vector3(0, 0, 0),
-            new THREE.Vector3(0, 0, -1)
-        ]);
-
-        const line = new THREE.Line(geometry);
-        line.name = 'ray';
-        line.scale.z = 10;
-
-        const geometry2 = new THREE.SphereGeometry(0.03, 8, 6);
-        const material = new THREE.MeshBasicMaterial({ color: 0xFF0000 });
-
-        const controllers = [];
-
-        for (let i = 0; i <= 1; i++) {
-            const controller = this.renderer.xr.getController(i);
-            controller.userData.index = i;
-            controller.userData.selectPressed = false;
-            controller.add(line.clone());
-            controller.userData.marker = this.createMarker(geometry2, material);
-            controllers.push(controller);
-            this.dolly.add(controller);
-
-            const grip = this.renderer.xr.getControllerGrip(i);
-            grip.add(controllerModelFactory.createControllerModel(grip));
-            this.dolly.add(grip);
-        }
-
-        return controllers;
     }
 
     setupXR() {
         this.renderer.xr.enabled = true;
-
-        const self = this;
-
-        function onSelectStart() {
-            this.userData.selectPressed = true;
-
-            if (this.userData.teleport) {
-                self.player.object.position.copy(this.userData.teleport.position);
-            } else if (this.userData.interactable) {
-                this.userData.interactable.play();
-            } else if (this.userData.marker.visible) {
-                const pos = this.userData.marker.position;
-                console.log(`${pos.x.toFixed(3)}, 
-                    ${pos.y.toFixed(3)},
-                    ${pos.z.toFixed(3)}`);
-            }
-        }
-
-        function onSelectEnd() {
-            this.userData.selectPressed = false;
-        }
-
-        function onSqueezeStart() {
-            this.userData.squeezePressed = true;
-            self.teleports.forEach(teleport => teleport.fadeIn(1));
-        }
-
-        function onSqueezeEnd() {
-            this.userData.squeezePressed = false;
-            self.teleports.forEach(teleport => teleport.fadeOut(1));
-        }
-
-        const btn = new VRButton(this.renderer);
-
-        this.controllers = this.buildControllers();
-
-        this.controllers.forEach(controller => {
-            controller.addEventListener('selectstart', onSelectStart);
-            controller.addEventListener('selectend', onSelectEnd);
-            controller.addEventListener('squeezestart', onSqueezeStart);
-            controller.addEventListener('squeezeend', onSqueezeEnd);
-        });
-
-        this.collisionObjects = [this.navmesh];
-
-        this.teleports.forEach(teleport =>
-            self.collisionObjects.push(teleport.children[0]));
-
-        this.interactables.forEach(Interactable => self.collisionObjects.push(interactable.mesh));
+        document.body.appendChild(VRButton.create(this.renderer));
     }
 
-    intersectObjects(controller) {
-        const line = controller.getObjectByName('ray');
-        this.workingMatrix.identity().extractRotation(controller.matrixWorld);
+    loadStaticModels() {
 
-        this.raycaster.ray.origin.setFromMatrixPosition(controller.matrixWorld);
-        this.raycaster.ray.direction.set(0, 0, -1).applyMatrix4(this.workingMatrix);
-
-        const intersects = this.raycaster.intersectObjects(this.collisionObjects);
-        const marker = controller.userData.marker;
-        marker.visible = false;
-
-        controller.userData.teleport = undefined;
-
-        if (intersects.length > 0) {
-            const intersect = intersects[0];
-            line.scale.z = intersect.distance;
-
-            if (intersect.object === this.navmesh) {
-                marker.scale.set(1, 1, 1);
-                marker.position.copy(intersect.point);
-                marker.visible = true;
-            } else if (intersect.object.parent && intersect.object.parent instanceof TeleportMesh) {
-                intersect.object.parent.selected = true;
-                controller.userData.teleport = intersect.object.parent;
-            } else {
-                const tmp = this.interactables.filter(interactable =>
-                    interactable.mesh == intersect.object);
-                if (tmp.length > 0) controller.userData.interactable = tmp[0];
-            }
-        }
-    }
-
-    createPlayer() {
-        const target = new THREE.Object3D();
-        target.position.set(-3, 0.25, 2);
-
-        const options = {
-            object: target,
-            speed: 5,
-            app: this,
-            name: 'player',
-            npc: false
-        };
-
-        const player = new Player(options);
-
-        this.dolly = new THREE.Object3D();
-        this.dolly.position.set(0, -0.25, 0);
-        this.dolly.add(this.camera);
-
-        this.dummyCam = new THREE.Object3D();
-        this.camera.add(this.dummyCam);
-
-        target.add(this.dolly);
-
-        this.dolly.rotation.y = Math.PI;
-
-        return player;
     }
 
     resize() {
@@ -368,31 +139,7 @@ class App {
     }
 
     render() {
-        const dt = this.clock.getDelta();
-        const self = this;
-
-        this.sun.position.copy(this.dummyCam.position);
-        this.sun.position.y += 10;
-        this.sun.position.z += 10;
-
-        this.stats.update();
-
-        if (this.renderer.xr.isPresenting) {
-
-            this.teleports.forEach(teleport => {
-                teleport.selected = false;
-                teleport.update();
-            });
-
-            this.controllers.forEach(controller => {
-                self.intersectObjects(controller);
-            });
-
-            this.interactables.forEach(interactable => interactable.update(dt));
-
-            this.player.update(dt);
-        }
-
+        window.requestAnimationFrame(render);
         this.renderer.render(this.scene, this.camera);
     }
 }
