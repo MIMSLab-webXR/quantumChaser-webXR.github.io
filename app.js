c898b449786fc7e7741ba8e783e1e01ca1bad6dd
pratiklp <37157850+pratiklp@users.noreply.github.com>
2021-02-26 11:49:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,76 +1,119 @@
-import * as THREE from './Library/THREE/three.module.js';
-import { OrbitControls } from './Library/THREE/jsm/OrbitControls.js';
-import { GLTFLoader } from './Library/THREE/jsm/GLTFLoader.js';
-import { FBXLoader } from './Library/THREE/jsm/FBXLoader.js';
-import { VRButton } from './Library/THREE/jsm/VRButton.js';
-import { ARButton } from './Library/THREE/jsm/ARButton.js';
-import { XRControllerModelFactory } from './Library/THREE/jsm/XRControllerModelFactory.js';
-import { BoxLineGeometry } from './Library/THREE/jsm/BoxLineGeometry.js';
-import { Stats } from './Library/stats.module.js';
-import { LoadingBar } from './Library/LoadingBar.js';
-import { vector3ToString } from './Library/DebugUtils.js';
-import { CanvasUI } from './Library/CanvasUI.js';
-import { CanvasKeyboard } from './Library/CanvasKeyboard.js';
-import { XRWorldMeshes } from './Library/XRWorldMeshes.js';
-import { TeleportMesh } from './Library/TeleportMesh.js';
-import { Player } from './Library/Player.js';
-import { RGBELoader } from './Library/THREE/jsm/RGBELoader.js';
-import { Interactable } from './Library/Interactable.js';
-import {
-    Constants as MotionControllerConstants,
-    fetchProfile,
-    MotionController
-} from './Library/THREE/jsm/motion-controllers.module.js';
+import * as THREE from '../build/three.module.js';
 
-class App {
-    constructor() {
-        this.init();
-        this.animate();
-    }
+import Stats from './jsm/libs/stats.module.js';
 
-    init() {
-        var container = document.createElement('div');
-        document.body.appendChild(container);
+import { OrbitControls } from './jsm/controls/OrbitControls.js';
+import { OBJLoader } from './jsm/loaders/OBJLoader.js';
 
-        var scene = new THREE.Scene();
-        scene.background = new THREE.Color("#808080");
+let container, stats;
 
-        var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        camera.position.set(0, 1.6, 3);
+let camera, scene, renderer;
 
-        var controls = new OrbitControls(camera, container);
-        controls.target.set(0, 1.6, 0);
-        controls.update();
+let pointLight;
 
-        var floor = {};
-        floor.geometry = new THREE.PlaneBufferGeometry(4, 4);
-        floor.material = new THREE.MeshStandardMaterial({
-            color: "#eeeeee",
-            roughness: 1,
-            metalness: 0
-        });
-        floor.mesh = new THREE.Mesh(floor.geometry, floor.material);
-        floor.mesh.rotation.x = -Math.PI / 2;
-        floor.mesh.receiveShadow = true;
-        scene.add(floor);
+init();
+animate();
 
-        var renderer = new THREE.WebGLRenderer();
-        renderer.setPixelRatio(window.devicePixelRatio);
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        renderer.outputEncoding = new THREE.sRGBEncoding;
-        renderer.shadowMap.enabled = true;
-        renderer.xr.enabled = true;
-        document.body.appendChild(renderer.domElement);
-    }
+function init() {
 
-    xrSetup() {
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
 
-    }
+    camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 5000 );
+    camera.position.z = 2000;
 
-    animate() {
-        requestAnimationFrame(animate);
-        renderer.render(scene, camera);
-    }
+    //cubemap
+    const path = 'textures/cube/SwedishRoyalCastle/';
+    const format = '.jpg';
+    const urls = [
+        path + 'px' + format, path + 'nx' + format,
+        path + 'py' + format, path + 'ny' + format,
+        path + 'pz' + format, path + 'nz' + format
+    ];
+
+    const reflectionCube = new THREE.CubeTextureLoader().load( urls );
+    const refractionCube = new THREE.CubeTextureLoader().load( urls );
+    refractionCube.mapping = THREE.CubeRefractionMapping;
+
+    scene = new THREE.Scene();
+    scene.background = reflectionCube;
+
+    //lights
+    const ambient = new THREE.AmbientLight( 0xffffff );
+    scene.add( ambient );
+
+    pointLight = new THREE.PointLight( 0xffffff, 2 );
+    scene.add( pointLight );
+
+    //materials
+    const cubeMaterial3 = new THREE.MeshLambertMaterial( { color: 0xff6600, envMap: reflectionCube, combine: THREE.MixOperation, reflectivity: 0.3 } );
+    const cubeMaterial2 = new THREE.MeshLambertMaterial( { color: 0xffee00, envMap: refractionCube, refractionRatio: 0.95 } );
+    const cubeMaterial1 = new THREE.MeshLambertMaterial( { color: 0xffffff, envMap: reflectionCube } );
+
+    //models
+    const objLoader = new OBJLoader();
+
+    objLoader.setPath( 'models/obj/walt/' );
+    objLoader.load( 'WaltHead.obj', function ( object ) {
+
+        const head = object.children[ 0 ];
+
+        head.scale.multiplyScalar( 15 );
+        head.position.y = - 500;
+        head.material = cubeMaterial1;
+
+        const head2 = head.clone();
+        head2.position.x = - 900;
+        head2.material = cubeMaterial2;
+
+        const head3 = head.clone();
+        head3.position.x = 900;
+        head3.material = cubeMaterial3;
+
+        scene.add( head, head2, head3 );
+
+    } );
+
+    //renderer
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    //controls
+    const controls = new OrbitControls( camera, renderer.domElement );
+    controls.enableZoom = false;
+    controls.enablePan = false;
+    controls.minPolarAngle = Math.PI / 4;
+    controls.maxPolarAngle = Math.PI / 1.5;
+
+    //stats
+    stats = new Stats();
+    container.appendChild( stats.dom );
+
+    window.addEventListener( 'resize', onWindowResize );
+
 }
 
-export { App };
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function animate() {
+
+    requestAnimationFrame( animate );
+    render();
+
+}
+
+function render() {
+
+    renderer.render( scene, camera );
+    stats.update();
+
+}
